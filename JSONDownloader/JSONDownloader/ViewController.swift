6011f973afaa691977003bdcbe829f8265b5e704
ahmetbostanciklioglu <40151328+ahmetbostanciklioglu@users.noreply.github.com>
2022-07-31 20:43:12 +0300

    Add files via upload

---
+++ JSONDownloader/JSONDownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONDownloader
+//
+//  Created by Ahmet Bostancıklıoğlu on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
